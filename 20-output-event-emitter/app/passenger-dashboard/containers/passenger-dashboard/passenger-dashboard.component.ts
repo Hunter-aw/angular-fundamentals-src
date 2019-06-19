f73192110900636921b6068a2969fec24fbc0d59
Hunter Weinsheink <hunteraweinsheink@gmail.com>
2019-06-19 10:27:42 +0300

    services in dep inject

--- 20-output-event-emitter/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ 20-output-event-emitter/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -1,74 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { Passenger } from '../../models/passenger.interface';
+import { Passenger } from "../../models/passenger.interface";
+
+import { PassengerDashboardService } from "../../passenger-dashboard.service";
 
 @Component({
-  selector: 'passenger-dashboard',
-  styleUrls: ['passenger-dashboard.component.scss'],
+  selector: "passenger-dashboard",
+  styleUrls: ["passenger-dashboard.component.scss"],
   template: `
     <div>
-      <passenger-count
-        [items]="passengers">
-      </passenger-count>
-      <div *ngFor = "let passenger of passengers;">
+      <passenger-count [items]="passengers"> </passenger-count>
+      <div *ngFor="let passenger of passengers">
         {{ passenger.fullname }}
       </div>
       <passenger-detail
-        *ngFor="let passenger of passengers;"
+        *ngFor="let passenger of passengers"
         [detail]="passenger"
         (edit)="handleEdit($event)"
-        (remove)="handleRemove($event)">
+        (remove)="handleRemove($event)"
+      >
       </passenger-detail>
     </div>
   `
 })
 export class PassengerDashboardComponent implements OnInit {
   passengers: Passenger[];
-  constructor() {}
+  constructor(private passengerService: PassengerDashboardService) {}
   ngOnInit() {
-    this.passengers = [{
-      id: 1,
-      fullname: 'Stephen',
-      checkedIn: true,
-      checkInDate: 1490742000000,
-      children: null
-    }, {
-      id: 2,
-      fullname: 'Rose',
-      checkedIn: false,
-      checkInDate: null,
-      children: [{ name: 'Ted', age: 12 },{ name: 'Chloe', age: 7 }]
-    }, {
-      id: 3,
-      fullname: 'James',
-      checkedIn: true,
-      checkInDate: 1491606000000,
-      children: null
-    }, {
-      id: 4,
-      fullname: 'Louise',
-      checkedIn: true,
-      checkInDate: 1488412800000,
-      children: [{ name: 'Jessica', age: 1 }]
-    }, {
-      id: 5,
-      fullname: 'Tina',
-      checkedIn: false,
-      checkInDate: null,
-      children: null
-    }];
+    this.passengers = this.passengerService.getPassengers();
   }
   handleEdit(event: Passenger) {
     this.passengers = this.passengers.map((passenger: Passenger) => {
       if (passenger.id === event.id) {
-        passenger = {...event}
+        passenger = { ...event };
       }
       return passenger;
-    })
+    });
   }
   handleRemove(event: Passenger) {
-    this.passengers = this.passengers.filter((passenger:Passenger) => {
-      return passenger.id !== event.id
+    this.passengers = this.passengers.filter((passenger: Passenger) => {
+      return passenger.id !== event.id;
     });
   }
 }
